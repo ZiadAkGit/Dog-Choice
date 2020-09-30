8ba9222b89e98c0d9282680b64611af225dbd2c2
Ziad AK <ziadakgit@gmail.com>
2020-09-30 18:43:44 +0300

    App is beta working

--- app/src/main/java/ziad/ak/wheelofdishes/MainActivity.java
+++ app/src/main/java/ziad/ak/wheelofdishes/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.btn1);
         final TextView txt = (TextView) findViewById(R.id.textView);
