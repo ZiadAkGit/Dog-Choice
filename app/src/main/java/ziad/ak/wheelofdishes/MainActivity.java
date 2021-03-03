3ec7c44a43d2b7c687ef65eef6f20bbf8fa37dc4
Ziad AK <ziadakgit@gmail.com>
2021-03-03 21:49:41 +0200

    Working HashMaps with dog names and attributes

--- app/src/main/java/ziad/ak/wheelofdishes/MainActivity.java
+++ app/src/main/java/ziad/ak/wheelofdishes/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,30 +15,73 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
-
-    int[] dogArts;
+    int choice = 0;
+    HashMap<String, HashMap<String,String>> dogS = new HashMap<String, HashMap<String,String>>();
+    HashMap<String,String> dogSS = new HashMap<String,String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn1 = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button2);
+        Button btn3 = findViewById(R.id.button3);
+        Button btn4 = findViewById(R.id.button6);
+        Button btn5 = findViewById(R.id.button5);
+        Button btn6 = findViewById(R.id.button4);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choice = 1;
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choice = 2;
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choice = 5;
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choice = 4;
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choice = 3;
+            }
+        });
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 myRef.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Log.d("Choice is", String.valueOf(choice));
                         for (DataSnapshot dog : snapshot.getChildren()) {
-                            String dogArt = dog.getValue().toString();
-                            Log.d("Dog is: " + dog.getKey() , dogArt);
+                            String[] dogVal = dog.getValue().toString().split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+                            for (int i = 0; i < dogVal.length - 1; i++){
+                                String dogVal1 = dogVal[i];
+                                if(dogVal1.length()==1){
+                                    dogSS.put(dogVal[i-1],dogVal1);
+                                    dogS.put(dog.getKey(),dogSS);
+                                }
+                            }
+                            Log.d("HASHMAP IS", String.valueOf(dogS));
                         }
                     }
                     @Override
